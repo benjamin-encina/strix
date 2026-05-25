e1e68940c4316c4c91fbfdfdd16308ac61c90dea
Benjamin Encina <be.encina@duocuc.cl>
2026-05-24 23:44:31 -0400

    feat(ms-tickets): añadir el GlobalExceptionHandler y los logs de SLF4J en todas las transiciones de estado

---
+++ ms-tickets/src/main/java/com/strix/mstickets/exception/RecursoNoEncontradoException.java
@@ -0,0 +1,10 @@
+package com.strix.mstickets.exception;
+
+/**
+ * HTTP 404 obtenido cuando un recurso no existe en la base de datos.
+ */
+public class RecursoNoEncontradoException extends RuntimeException {
+    public RecursoNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+}
